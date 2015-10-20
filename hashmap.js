722da71e91c702b9e85bccf118f40c427f9cd313
Spencer Brown <me@spencer.sx>
2015-10-20 17:35:02 -0400

    Add `hashString`

--- hashmap.js
+++ hashmap.js
@@ -14,6 +14,30 @@ var Hashmap = function(size) {
     };
 
     /**
+     * Generate hash from given string. Based on http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/.
+     * A more complex hashing function would be used (like https://github.com/garycourt/murmurhash-js/blob/master/murmurhash3_gc.js) if external
+     * libraries were able to be included.
+     * @param {string} str = String from which hash should be generated
+     * @returns {number} Hash
+     */
+    function hashString(str) {
+        var hash = 0;
+
+        if (str.length === 0) {
+            return hash;
+        }
+
+        var char;
+        for (i = 0; i < str.length; i++) {
+            char = str.charCodeAt(i);
+            hash = ((hash << 5) - hash) + char;
+            hash = hash & hash; // convert to 32-bit integer
+        }
+
+        return hash;
+    }
+
+    /**
      * Returns the value of the value associated with the given key, or null if no value is set.
      * @param {string} key - The key to be looked up
      * @returns {object} Value associated with given key or null
